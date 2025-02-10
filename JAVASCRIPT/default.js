6afa5e77c945a81d4b93130cf11adc76a95d3a23
Koviricsi <richard.kovi.07@gmail.com>
2025-02-10 22:21:13 +0100

    popcorn

--- JAVASCRIPT/default.js
+++ JAVASCRIPT/default.js
@@ -15,3 +15,53 @@ function navAnimate(){
     }
     navprogress -= 0.01;
 }
+
+let popcorn = document.getElementsByClassName("popcorn");
+for (let i = 0; i < popcorn.length; i++){
+    popcorn[i].style.left = "100%";
+    popcorn[i].style.top = "50%";
+    popcorn[i].style.rotate = "180deg";
+}
+setInterval(function(){
+    for (let i = 0; i < popcorn.length; i++){
+        let left = Math.round(Math.random()*100) + "%"
+        let top = Math.round(Math.random()*100) + "%";
+        let rotate = Math.round(Math.random()*180*Math.random())*5 + "deg";
+        let move = setInterval(function(){
+            let moveon = [false, false, false]
+            let corn = popcorn[i].style;
+
+            if (parseInt(corn.left) < parseInt(left)){
+                corn.left = parseInt(corn.left) + 1 + "%";
+            } else if (parseInt(corn.left) > parseInt(left)){
+                corn.left = parseInt(corn.left) - 1 + "%";
+            }
+            else{
+                moveon[0] = true;
+            }
+
+            if (parseInt(corn.top) < parseInt(top)){
+                corn.top = parseInt(corn.top) + 1 + "%";
+            } else if (parseInt(corn.top) > parseInt(top)){
+                corn.top = parseInt(corn.top) - 1 + "%";
+            }
+            else{
+                moveon[1] = true;
+            }
+
+            if (parseInt(corn.rotate) < parseInt(rotate)){
+                corn.rotate = parseInt(corn.rotate) + 1 + "deg";
+            } else if (parseInt(corn.rotate) > parseInt(rotate)){
+                corn.rotate = parseInt(corn.rotate) - 1 + "deg";
+            }
+            else{
+                moveon[2] = true;
+            }
+
+            if (moveon[0] && moveon[1]){
+                clearInterval(move);
+            }
+        },10)
+    }
+    
+}, 1000)

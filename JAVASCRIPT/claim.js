5ef687b9cfd86ef7198e1b05c5fdc7500ced2ab8
Koviricsi <richard.kovi.07@gmail.com>
2025-02-13 19:02:18 +0100

    asaad

--- JAVASCRIPT/claim.js
+++ JAVASCRIPT/claim.js
@@ -1,9 +1,23 @@
 let room = document.getElementById("room");
+let number = 0;
 
-for (let i = 1; i <= 60; i++){
-    let chair = document.createElement("section");
-    chair.style.gridArea = "a" + i;
-    chair.setAttribute("number", i);
-    chair.innerHTML = i;
-    room.appendChild(chair);
+for (let i = 3; i <= 6; i++){
+    for (let j = 3; j <= 14; j++){
+        number++;
+        let chair = document.createElement("section");
+        chair.style.gridArea = i+"/"+j;
+        chair.setAttribute("number", number);
+        chair.innerHTML = number;
+        room.appendChild(chair);
+    }
+}
+for (let i = 7; i <= 8; i++){
+    for (let j = 1; j <= 16; j++){
+        number++;
+        let chair = document.createElement("section");
+        chair.style.gridArea = i+"/"+j;
+        chair.setAttribute("number", number);
+        chair.innerHTML = number;
+        room.appendChild(chair);
+    }
 }
